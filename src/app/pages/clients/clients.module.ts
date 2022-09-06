ed7058bf6f044822c393c51a884c5d22d8dbe602
OscarRamirez <al221310583@gmail.com>
2022-09-06 00:29:54 -0500

    :heavy_plus_sign: added modules material

--- src/app/pages/clients/clients.module.ts
+++ src/app/pages/clients/clients.module.ts
@@ -13,12 +13,20 @@ import {MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSortModule } from '@angular/material/sort';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule as ngFormsModule } from '@angular/forms';
+import {BidiModule} from '@angular/cdk/bidi';
+import { ClientCreateComponent } from './client-create/client-create.component';
 @NgModule({
   declarations: [
     ClientsComponent,
     ClientUpdateComponent,
+    ClientCreateComponent,
     ClientMainComponent,
-
   ],
   imports: [
     CommonModule,
@@ -34,7 +42,14 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatFormFieldModule,
     MatInputModule,
     MatSortModule,
-    MatDialogModule
+    BidiModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatCardModule,
+    MatCheckboxModule,
+    ngFormsModule,
 
   ],
 
