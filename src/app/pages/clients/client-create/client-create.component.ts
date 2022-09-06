258070772334e57f5c2fcbaa2b4b16eba991beca
OscarRamirez <al221310583@gmail.com>
2022-09-06 00:28:22 -0500

    :necktie: added modal client

--- src/app/pages/clients/client-create/client-create.component.ts
+++ src/app/pages/clients/client-create/client-create.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../client-main/client-main.component';
 
 @Component({
   selector: 'ngx-client-create',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ClientCreateComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
